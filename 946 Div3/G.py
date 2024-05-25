d4180528c2d386e6a81587f32e8ddf5d98cd5069
Muhammad Saad <saad.salman11123@gmail.com>
2024-05-25 13:00:19 +0500

    Day 008/100
    Solved 946 Div3 - G

--- 946 Div3/G.py
+++ 946 Div3/G.py
@@ -1,31 +1,18 @@
 import heapq
-
 for __ in range(int(input())):
     m, x = map(int, input().split())
     c = list(map(int, input().split()))
 
     h = []
     cur = 0
+
     for i in range(m):
-        if not h and c[i] > cur:
-            cur += x
-            continue
-        elif c[i] <= cur:
+        if c[i] <= cur:
             heapq.heappush(h, -c[i])
             cur -= c[i]
-        elif c[i] > cur:
-            mx = heapq.heappop(h)
-            cur -= mx
-
-            if c[i] > cur:
-                heapq.heappush(h, mx)
-                cur += mx
-                cur += x
-                continue
-            else:
-                heapq.heappush(h, -c[i])
-                cur -= c[i]      
-
+        elif h and -h[0] > c[i]:
+            cur += -heapq.heappop(h) - c[i]
+            heapq.heappush(h, -c[i])
         cur += x
     
     print(len(h))
